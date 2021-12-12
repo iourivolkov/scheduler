740d88058791ebe23246aca340a55a737eb0d645
iouri volkov <volk.iouri@gmail.com>
2021-12-12 10:29:12 +0800

    create InterviewerListItem component and add InterviewerListItem stories to index.js

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const InterviewerListItem = function() {
+
+  return (
+    <li className="interviewers__item">
+  <img
+    className="interviewers__item-image"
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  Sylvia Palmer
+</li>
+  )
+}
+
+
+
+
+export default InterviewerListItem;
