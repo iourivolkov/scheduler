f9adee40ff2cba2273f0b2d92e032b9d3533741e
iouri volkov <volk.iouri@gmail.com>
2021-12-13 06:45:04 +0800

    create Confirm component

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'components/Button';
+
+export default function Confirm(props) {
+
+  return (
+    <main className="appointment__card appointment__card--confirm">
+  <h1 className="text--semi-bold">Delete the appointment?</h1>
+  <section className="appointment__actions">
+    <Button danger>Cancel</Button>
+    <Button danger>Confirm</Button>
+  </section>
+</main>
+  )
+}
+
+// confirm component should accept the following props;
+// message: str --> e.g "delete the component"
+// onConfirm = f(x) --> call when the user clicks the confirm button
+// onCancel = f(x) --> call whe the user clicks the cancel button 
--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Appointment from "../src/components/Appointment/index";
 import Header from '../src/components/Appointment/Header';
 import Empty from '../src/components/Appointment/Empty';
 import Show from '../src/components/Appointment/Show';
+import Confirm from '../src/components/Appointment/Confirm';
+
 
 
 
@@ -149,5 +151,6 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
-  .add("Show", () => <Show onEdit={action("onEdit")}/>);
+  .add("Show", () => <Show onEdit={action("onEdit")}/>)
+  .add("Confirm", () => <Confirm />);
 
