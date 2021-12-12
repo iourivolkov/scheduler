815a840e1baecb848c7da201c49c757bcfeedebf
iouri volkov <volk.iouri@gmail.com>
2021-12-12 10:45:35 +0800

    refactor InterviewerListItem component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import "components/InterviewerListItem.scss";
+import classNames from 'classnames';
 
-const InterviewerListItem = function() {
+const InterviewerListItem = function(props) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  })
 
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={props.setInterviewer}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
-</li>
-  )
+    {props.selected && props.name}
+    </li>
+  );
 }
 
 
 
 
 export default InterviewerListItem;
+
+// a single interviewer is {
+  // id: 1, 
+  // name: 'jane doe',
+  // avatar: img file
+// }
