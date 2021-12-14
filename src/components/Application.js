60fa84086bc4e32d678a0af1aa1dccad7452c0b4
iouri volkov <volk.iouri@gmail.com>
2021-12-13 23:23:19 -0500

    Add proxy

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
+import Appointment from "./Appointment";
+import "components/Application.scss";
 
 const days = [
   {
@@ -30,9 +32,49 @@ const interviewers = [
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 export default function Application(props) {
 
   const [ day, setDay ] = useState("Monday");
+
 
   return (
     <main className="layout">
@@ -57,20 +99,20 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"></nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+     {appointments.map(appointment => (
+       <Appointment
+       key={appointment.id}
+       {...appointment}
+       />
+     ))}
+     <Appointment key="last" time="5pm" />
       </section>
       
     </main>
   );
 }
+
+// Appointment component accepts 3 props: 
+// id 
+// time
+// interview 
