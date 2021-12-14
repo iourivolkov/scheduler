4bf8e09004efe959dc1f8e5b0d568b2170176446
iouri volkov <volk.iouri@gmail.com>
2021-12-14 09:53:45 -0500

    Combine state for day, days into a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,9 +57,10 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [ day, setDay ] = useState("Monday");
-
-  const [ days, setDays ] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
 
   useEffect(() => {
     axios.get("/api/days").then(response => setDays(response.data));
---
+++ src/helpers/selectors.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+export const getAppointmentsForDay = (state, day) => {
+  const dayObject = state.days.find(element => element.name === day);
+ 
+  if(!dayObject) {
+    return [];
+  }
+}
+
+const appointmentIds = dayObject.appointments;
+
+const appointmentsForDay = [];
+
+for(const id in state.appointments) {
+  if(appointmentIds.includes(Number(id))) {
+    appointmentsForDay.push(state.appointments[id])
+  }
+}
