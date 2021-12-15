ad8ebb2e385f20a57808a7a5101cb3fc58084f99
iouri volkov <volk.iouri@gmail.com>
2021-12-15 15:13:07 -0500

    implement useVisualMode with initial mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function useVisualMode() {
-  
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial)
 
-
+  return {mode};
 }
