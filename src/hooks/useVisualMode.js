a58580272809c6663d01108e3e258b3209ca29bf
iouri volkov <volk.iouri@gmail.com>
2021-12-15 15:35:01 -0500

    add history array to useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial]);
+
+  
   const transition = (newMode) => {
     setMode(newMode);
   }
