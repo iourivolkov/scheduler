82e882dda4fde3859736c4af5564f43e3165fb63
iouri volkov <volk.iouri@gmail.com>
2021-12-14 20:59:19 -0500

    Modify effect method to do third request using promise.all for interviewers and update interviewers state with the response

--- src/components/Application.js
+++ src/components/Application.js
@@ -79,13 +79,15 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
      axios.get('/api/days'),
-      axios.get('/api/appointments')
+     axios.get('/api/appointments'),
+     axios.get('/api/interviewers')
 
   ]).then((all) => {
-    setState(state => ({...state, days: all[0].data, appointments: all[1].data}));
+    setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
   });
 }, []);
 
+console.log(state.interviewers);
 
   return (
     <main className="layout">
