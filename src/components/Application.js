7c571dccbf23266004be29857c36b69f40dab926
iouri volkov <volk.iouri@gmail.com>
2021-12-15 13:01:53 -0500

    Add tests for interview data, transform interview data, update Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,53 +6,14 @@ import InterviewerList from "./InterviewerList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
 import axios from 'axios';
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   }
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 // ];
 
 
@@ -65,16 +26,9 @@ export default function Application(props) {
     interviewers: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-
   // function takes in day param and copies state object + updates day
   const setDay = day => setState({...state, day});
   // const setDays = days => setState(prev => ({...prev, days}))
-  
-
-  // useEffect(() => {
-  //   axios.get("/api/days").then(response => setDays(response.data)
-  // )}, [])
 
   useEffect(() => {
     Promise.all([
@@ -83,11 +37,26 @@ export default function Application(props) {
      axios.get('/api/interviewers')
 
   ]).then((all) => {
-    setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
   });
 }, []);
 
-console.log(state.interviewers);
+const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+        interview={interview}
+      />
+    )
+  })
+
+// console.log(schedule);
+
 
   return (
     <main className="layout">
@@ -112,12 +81,6 @@ console.log(state.interviewers);
 />
       </section>
       <section className="schedule">
-     {dailyAppointments.map(appointment => (
-       <Appointment
-       key={appointment.id}
-       {...appointment}
-       />
-     ))}
      <Appointment key="last" time="5pm" />
       </section>
       
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -19,5 +19,28 @@ for(const id in state.appointments) {
 return appointmentsForDay;
 }
 
+// function will return an object that contains the interview data if it is passed an object that contains an interviewer
+// interview = obj that contains interviewer
+export const getInterview = (state, interview) => {
+
+  if(!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+
+  for(const id in state.interviewers) {
+    if(Number(id) === interviewerId) {
+      
+      return (
+        {
+          student: interview.student,
+          interviewer: state.interviewers[id]
+        }
+      )
+    }
+  }
+}
+
 
 
