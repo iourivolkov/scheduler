fb71d8b9edf3fb47e2105940ba714f32ecab1913
iouri volkov <volk.iouri@gmail.com>
2021-12-15 16:00:21 -0500

    implement back function in useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,13 +4,23 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
   const [history, setHistory] = useState([initial]);
 
-  
+
   const transition = (newMode) => {
     setMode(newMode);
+    setHistory(history => [...history, newMode]);
   }
+
   const back = () => {
+    setHistory(history => {
+      const newItem = [...history].slice(0,-1);
+      setMode(newItem[newItem.length - 1]);
+      return newItem;
+    })
 
   }
 return {mode, transition, back}
 }
+
+// when transition is called - need to add the new mode to our history
+// when back is called, we should set the mode to the prev item in our history array 
 
