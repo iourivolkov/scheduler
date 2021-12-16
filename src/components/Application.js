76aba89280354c7a1c64ec252260d84369f31ce2
iouri volkov <volk.iouri@gmail.com>
2021-12-15 20:17:43 -0500

    Add missing {schedule} section to schedule class

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,13 +8,13 @@ import "components/Application.scss";
 import axios from 'axios';
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-// ];
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 
 
 export default function Application(props) {
@@ -32,9 +32,9 @@ export default function Application(props) {
 
   useEffect(() => {
     Promise.all([
-     axios.get('/api/days'),
-     axios.get('/api/appointments'),
-     axios.get('/api/interviewers')
+     axios.get('http://localhost:8001/api/days'),
+     axios.get('http://localhost:8001/api/appointments'),
+     axios.get('http://localhost:8001/api/interviewers')
 
   ]).then((all) => {
     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
@@ -81,6 +81,7 @@ const appointments = getAppointmentsForDay(state, state.day);
 />
       </section>
       <section className="schedule">
+        {schedule}
      <Appointment key="last" time="5pm" />
       </section>
       
