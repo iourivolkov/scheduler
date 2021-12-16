32cd2af306450e68bb419882aa739ea9cfe5af0f
iouri volkov <volk.iouri@gmail.com>
2021-12-15 20:41:36 -0500

    use back function to return to the empty state when cancel btn is clicked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -29,7 +29,8 @@ export default function Appointment(props) {
      />
      )}
      {mode === CREATE && (
-       <Form interviewers={[]}/>
+       <Form interviewers={[]}
+       onCancel={back}/>
      )}
    </article>
   )
