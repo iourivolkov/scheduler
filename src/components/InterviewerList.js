edaf4c3869724c5a937f838e5a706e165c0333de
iouri volkov <volk.iouri@gmail.com>
2021-12-27 15:07:19 -0500

    Install PropTypes as dependency, add PropTypes validation to InterviewerList function

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "components/InterviewerList.scss";
 import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 
 
 export default function InterviewerList(props) {
 
+  InterviewerList.propTypes= {
+    interviewers: PropTypes.array.isRequired
+  };
 
   const interviewers = props.interviewers.map((interviewer) => {
     return (
@@ -28,4 +32,3 @@ export default function InterviewerList(props) {
   );
 }
 
-// build a component that returns an array of InterviewerListItem components
