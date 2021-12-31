06494f27cbe93cd67e2b64e8ec1913adc38872c6
iouri volkov <volk.iouri@gmail.com>
2021-12-30 21:48:38 -0500

    refactor test 2

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,6 +4,11 @@ describe("Navigation", () => {
   });
 
   it("Should navigate to Tuesday", () => {
+
     cy.visit("/");
+
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 });
